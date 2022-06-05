f746b412f985513548d82ff16180c86bea5a2c0d
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-06-05 19:12:59 +0300

    Final

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Sayıyı tahmin et!");
+
+    let gizli_sayi = rand::thread_rng().gen_range(1..101);
+
+    println!("Gizli sayı : {}", gizli_sayi);
+
+    loop {
+        println!("Lütfen tahmininizi girin.");
+
+        let mut tahmin = String::new();
+
+        io::stdin()
+            .read_line(&mut tahmin)
+            .expect("Satır okunamadı!");
+
+        let tahmin: u32 = match tahmin.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Tahminin: {}", tahmin);
+
+        match tahmin.cmp(&gizli_sayi) {
+            Ordering::Less => println!("Çok küçük!"),
+            Ordering::Greater => println!("Çok büyük!"),
+            Ordering::Equal => {
+                println!("Doğru Bildin!");
+                break;
+            }
+        }
+    }
 }
